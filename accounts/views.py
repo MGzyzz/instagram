736fc1428206846ea344e567d2c71c99658a0a9c
Madi <madi_gaziz@mail.ru>
2023-08-01 23:44:39 +0600

    Для решение проблемы с созаднием пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import views, login
 from django.urls import reverse_lazy, reverse
 
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, RegisterForm
 from django.views.generic import CreateView
 from accounts.models import User
 
@@ -25,3 +25,17 @@ class AuthSuccessUrlMixin:
 class LoginView(views.LoginView):
     template_name = 'login.html'
     form_class = LoginForm
+
+
+class RegisterView(AuthSuccessUrlMixin, CreateView):
+    model = User
+    template_name = 'register.html'
+    form_class = RegisterForm
+
+    def form_valid(self, form):
+
+        user = form.save()
+
+        login(self.request, user)
+
+        return redirect(self.get_success_url())
