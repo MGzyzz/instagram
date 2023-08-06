730d592cd15a429c1a8974145ee399cc810cc6d2
Madi <madi_gaziz@mail.ru>
2023-08-07 02:41:18 +0600

    Создал пути для классов с views.py для передвижение по сайту

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('login', views.LoginView.as_view(), name='login'),
-    path('register', views.RegisterView.as_view(), name='register')
+    path('auth/login', views.LoginView.as_view(), name='login'),
+    path('auth/register', views.RegisterView.as_view(), name='register'),
+    path('auth/logout', views.Logout.as_view(), name='logout'),
+    path('profile/<int:id>/detail', views.Detail.as_view(), name='profile')
 ]
