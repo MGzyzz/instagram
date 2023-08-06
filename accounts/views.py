2e9e7aae2d47cca787b256de23d263e18246bfc6
Madi <madi_gaziz@mail.ru>
2023-08-07 02:42:27 +0600

    Создал классы для логин и регистер а так же реализовал класс Detail для просмотра профиля пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import views, login
-from django.urls import reverse_lazy, reverse
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib.auth import views, login, get_user_model
 from accounts.forms import LoginForm, RegisterForm
-from django.views.generic import CreateView
-from accounts.models import User
-
+from django.views.generic import CreateView, DetailView
+from accounts.models import User, Profile
+from instagram.models import Publication, Subscription
 
 class AuthSuccessUrlMixin:
     def get_success_url(self):
@@ -21,10 +21,15 @@ class AuthSuccessUrlMixin:
         return next_url
 
 
+class Logout(views.LogoutView):
+    def get_next_page(self):
+        return self.request.META.get('HTTP_REFERER')
 
-class LoginView(views.LoginView):
+
+class LoginView(AuthSuccessUrlMixin, views.LoginView):
     template_name = 'login.html'
     form_class = LoginForm
+
 
 
 class RegisterView(AuthSuccessUrlMixin, CreateView):
@@ -34,8 +39,49 @@ class RegisterView(AuthSuccessUrlMixin, CreateView):
 
     def form_valid(self, form):
 
+        avatar_file = self.request.FILES.get('avatar')
+        user_information = form.cleaned_data['user_information']
+        phone_number = form.cleaned_data['phone_number']
+        gender = form.cleaned_data['gender']
+
         user = form.save()
+
+        profile, created = Profile.objects.get_or_create(user=user)
+        profile.user_information = user_information
+        profile.phone_number = phone_number
+        profile.gender = gender
+        profile.avatar = avatar_file
+
+
+
+        if avatar_file:
+            profile.avatar = avatar_file
+        profile.save()
 
         login(self.request, user)
 
         return redirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        return redirect('register')
+
+
+class Detail(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user/detail.html'
+    context_object_name =  'user'
+    pk_url_kwarg = 'id'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.object
+        user_two = self.request.user
+        publication = Publication.objects.filter(user=user)
+        context['publications'] = publication
+        subscribe = Subscription.objects.filter(subscriber=user_two, followed_user=user)
+        context['subscribe'] = subscribe.exists()
+        return context
