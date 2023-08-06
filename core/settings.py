7ab2f5169648c363c2ba5afe1cb3e70abf249afc
Madi <madi_gaziz@mail.ru>
2023-08-07 02:59:13 +0600

    Внес небольшие изменение

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instagram',
     'accounts'
 ]
 
@@ -131,3 +132,5 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'home'
