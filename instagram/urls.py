0e0899f19c9d35776eff2fa247daa8db96c486bb
Madi <madi_gaziz@mail.ru>
2023-08-07 02:51:24 +0600

    Создал пути для передвижение по сайту

---
+++ instagram/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from instagram import views
+
+urlpatterns = [
+    path('', views.Home.as_view(), name='home'),
+    path('<int:id>/detail', views.PublicationUserDetail.as_view(), name='detail_publication'),
+    path('<int:id>/create_comment', views.UploadComments.as_view(), name='create-comments'),
+    path('create_publication', views.CreatePublication.as_view(), name='create-publication'),
+    path('<int:id>/like', views.Like.as_view(), name='like'),
+    path('<int:id>/subscribe', views.Subscribe.as_view(), name='subscribe')
+]
