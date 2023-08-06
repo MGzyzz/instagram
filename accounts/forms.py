5e532187ff056f08ae3b08e65155f3ac056b2874
Madi <madi_gaziz@mail.ru>
2023-08-07 02:39:55 +0600

    Реализовал систему логин с помощью username и email и сдела RegisterForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,19 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import password_validation, get_user_model
+from django.contrib.auth import get_user_model
 
-from accounts.models import User, Profile
+from accounts.models import User
 
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
         max_length=100,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control mb-3'
-            }
-        )
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3'})
     )
+
     password = forms.CharField(
         max_length=100,
         widget=forms.PasswordInput(
@@ -23,57 +20,82 @@ class LoginForm(AuthenticationForm):
         )
     )
 
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+
+            user_model = get_user_model()
+            if '@' in username:
+                user = user_model.objects.filter(email=username).first()
+            else:
+                user = user_model.objects.filter(username=username).first()
+
+            if user is not None and user.check_password(password):
+                self.user_cache = user
+            else:
+                self.add_error('username', 'Invalid login/email or password.')
+
+        return self.cleaned_data
+
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        label='Email Address',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your email',
+        })
+    )
+
+    avatar = forms.ImageField(
+        label='Avatars',
+        widget=forms.ClearableFileInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'avatar',
+        })
+    )
+
     first_name = forms.CharField(
-        required=True,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control mb-3'
-            }
-        ),
-        label='First Name',
+        label='Name',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'First Name'
+        })
     )
 
-    last_name = forms.CharField(
-        required=True,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control mb-3'
-            }
-        ),
-        label='Second Name'
+    user_information = forms.CharField(
+        label='User Information',
+        widget=forms.Textarea(attrs={
+            'class': 'textarea-width',
+            'placeholder': 'About me'
+        })
     )
 
-    password1 = forms.CharField(
-        label="Password",
-        strip=False,
-        widget=forms.PasswordInput(
-            attrs={
-                'autocomplete': 'new-password',
-                'class': 'form-control mb-3'
-            }
-        ),
-        help_text=password_validation.password_validators_help_text_html(),
+    phone_number = forms.CharField(
+        label='Phone_number',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}
+        )
     )
-    password2 = forms.CharField(
-        label="Password confirmation",
-        widget=forms.PasswordInput(
-            attrs={
-                'autocomplete': 'new-password',
-                'class': 'form-control mb-3'
-            }
-        ),
-        strip=False,
-        help_text="Enter the same password as before, for verification.",
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+
+    gender = forms.ChoiceField(
+        label='Gender',
+        choices=GENDER_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control mb-3'})
     )
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control mb-3'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control mb-3'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control mb-3'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control mb-3'}),
-        }
+        fields = ['username', 'email', 'avatar', 'password1', 'password2', 'first_name', 'user_information', 'phone_number', 'gender']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
